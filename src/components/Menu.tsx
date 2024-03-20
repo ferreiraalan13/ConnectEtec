a2b48ef79b525f5017d4745cfc6bcd8860aa2e7d
ferreiraalan13 <ferreiraalan13@gmail.com>
2024-03-20 15:12:05 -0300

    feat: ajustes menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,13 +5,13 @@ export default function Menu(){
     return(
         <div className="bg-gray-100 flex flex-col w-[276px] p-[15px] gap-[100px] h-screen rounded-[16px]">
             <div className="flex gap-2 flex-col w-3 w-full">
-                <button className='flex bg-gray-300 text-gray-700 items-center gap-2 '><img className='w-[50px] rounded-full bg-gray-300' src={userImage} alt="" />Fulano  <span className='text-gray-500'>@Fulano</span></button>
+                <button className='flex bg-gray-300 text-gray-700 items-center gap-3 p-2 rounded-lg h-{78px} '><img className='w-[50px] rounded-full bg-gray-300' src={userImage} alt="" />Fulano  <span className='text-gray-500'>@Fulano</span></button>
             </div>
             <div className="flex flex-col w-3 w-full gap-[10px] ">
-                <button className="bg-gray-300 text-gray-700 flex gap-2"> <Home/>Inicio</button>
-                <button className="bg-gray-300 text-gray-700 flex gap-2"> <Users/>Amigos</button>
-                <button className="bg-gray-300 text-gray-700 flex gap-2"> <PartyPopper/>Eventos</button>
-                <button className="bg-gray-300 text-gray-700 flex gap-2"> <Settings/>Configuracao</button>
+                <button className="bg-gray-300 text-gray-700 flex gap-3 p-2 rounded-lg"> <Home/>Inicio</button>
+                <button className="bg-gray-300 text-gray-700 flex gap-3 p-2 rounded-lg"> <Users/>Amigos</button>
+                <button className="bg-gray-300 text-gray-700 flex gap-3 p-2 rounded-lg"> <PartyPopper/>Eventos</button>
+                <button className="bg-gray-300 text-gray-700 flex gap-3 p-2 rounded-lg"> <Settings/>Configuracao</button>
             </div>
         </div>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import SignUp from './pages/sign-up/SignUp.tsx'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SignUp />
+    <App/>
+    
   </React.StrictMode>,
 )
