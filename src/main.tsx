0eac09c379441a2545d1b5e30391dc575fb69aca
ferreiraalan13 <ferreiraalan13@gmail.com>
2024-03-22 13:29:56 -0300

    feat: config main + chakra

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import SignUp from './pages/sign-up/SignUp.tsx'
 import App from './App.tsx'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App/>
-    
+    <ChakraProvider>
+      <App/>
+    </ChakraProvider>
   </React.StrictMode>,
 )
