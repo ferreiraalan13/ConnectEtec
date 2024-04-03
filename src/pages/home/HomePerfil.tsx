5082f60b9c91fcae18d6194266e1dbc986c0f5c8
ferreiraalan13 <ferreiraalan13@gmail.com>
2024-04-03 13:14:35 -0300

    Componente tela de perfil, home perfil

--- src/pages/home/HomePerfil.tsx
+++ src/pages/home/HomePerfil.tsx
@@ -1,44 +1,25 @@
-
-import Post from '../../components/Post';
-
-import { Box, SimpleGrid } from '@chakra-ui/react';
-import Evento from '../../components/Evento';
-import Menu from '../../components/Menu';
-import CriarPost from '../../components/CriarPost';
-import Perfil from '../../components/Perfil';
+import { Box } from "@chakra-ui/react";
+import Evento from "../../components/Evento";
+import Menu from "../../components/Menu";
+import Perfil from "../../components/Perfil";
 
 export default function HomePerfil() {
-
-
-  
   return (
-    <div className='bg-gray-300 flex flex-col gap-3 p-2' >
-      
-      <div className='flex gap-4'>
-        
-          <Menu/>
-        
-        
+    <div className="bg-gray-300 flex flex-col gap-3 p-2">
+      <div className="flex gap-4">
+        <Menu />
 
-        <Box className='flex gap-3 justify-center' w="1100px">
-          <div className='flex flex-col gap-3 '>
-            <Perfil/>
+        <Box className="flex gap-3 justify-center" w="1100px">
+          <div className="flex flex-col gap-3 ">
+            <Perfil />
           </div>
-          
-          
-          
-          
-          
         </Box>
-        
-        <div className='flex flex-col gap-3'>
-          <Evento/>
-          <Evento/> 
+
+        <div className="flex flex-col gap-3">
+          <Evento />
+          <Evento />
         </div>
-           
       </div>
-      
     </div>
-    
   );
 }
