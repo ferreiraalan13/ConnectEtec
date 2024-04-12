40f97c661f8bb266a7fc35a16b79a7cf7e60057f
ferreiraalan13 <ferreiraalan13@gmail.com>
2024-04-12 17:47:35 -0300

    ajustes menu 2

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,23 +4,18 @@ import { Box, Container, Flex } from "@chakra-ui/react";
 import Evento from "../../components/Evento";
 import Menu from "../../components/Menu";
 
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Home, PocketKnife, Search } from "lucide-react";
 import { useState } from "react";
+import ItensMenu from "../../components/ItensMenu";
 
 export default function App() {
   const [open, setOpen] = useState(true);
 
   return (
-    <Container
-      minW={"100%"}
-      p={0}
-      height={"100vh"}
-      overflow={"hidden"}
-      display={"flex"}
-    >
-      <Box
+    <Box height={"100vh"} overflow={"hidden"} className="flex bg-red-500">
+      <div
         className={`bg-purple-400 h-screen p-5 pt-8 ${
-          open ? "w-72" : "w-15"
+          open ? "w-72" : "w-20"
         } duration-300 relative`}
       >
         <ArrowLeft
@@ -29,8 +24,38 @@ export default function App() {
           }`}
           onClick={() => setOpen(!open)}
         />
-        <Menu />
-      </Box>
+
+        <div className="inline-flex">
+          <PocketKnife className="text-4xl cursor-pointer block float-left" />
+          <h1
+            className={`text-white origin-left font-medium text-2xl duration-300 ${
+              !open && "scale-0"
+            }`}
+          >
+            ConnectEtec
+          </h1>
+        </div>
+
+        <div
+          className={`flex items-center rounded-md bg-gray-700 mt-6 ${
+            !open ? "px-2.5" : "px-4"
+          } py-2`}
+        >
+          <Search
+            className={`text-white text-lg float-left cursor-pointer ${
+              open && "mr-2"
+            } `}
+          />
+          <input
+            type="search"
+            placeholder="Pesquisar"
+            className={`text-base text-white bg-transparent w-full focus:outline-none ${
+              !open && "hidden"
+            }`}
+          />
+        </div>
+      </div>
+
       <Box
         padding={"20px"}
         css={{
@@ -50,8 +75,7 @@ export default function App() {
         width={"100%"}
       >
         <Post />
-        <Evento />
       </Box>
-    </Container>
+    </Box>
   );
 }
