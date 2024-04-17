703af4a307f8edcc9932cafcf959f1440bde91c8
ferreiraalan13 <ferreiraalan13@gmail.com>
2024-04-17 15:01:16 -0300

    Configuração de cadastro e API

--- src/components/InputTemplate.tsx
+++ src/components/InputTemplate.tsx
@@ -4,19 +4,29 @@ interface Props {
   htmlFor?: string;
   name?: string;
   id?: string;
-  
+  required?: boolean;
+  onChange?: string;
 }
 
-
-export default function Input({ labelTitle, type, htmlFor, name, id }: Props) {
+export default function Input({
+  labelTitle,
+  type,
+  htmlFor,
+  name,
+  id,
+  required,
+}: Props) {
   return (
     <div className="flex flex-col gap-2 w-full">
-
-      <label htmlFor={htmlFor} className="text-sm font-medium text-gray-700">{labelTitle}</label>
-      <input name={name} id={id}
-
+      <label htmlFor={htmlFor} className="text-sm font-medium text-gray-700">
+        {labelTitle}
+      </label>
+      <input
+        name={name}
+        id={id}
         type={type}
         className="w-full px-2 rounded-xl border-2 border-gray-300 shadow-sm bg-gray-100 focus:border-red-500 focus:outline-none"
+        required={required}
       />
     </div>
   );
