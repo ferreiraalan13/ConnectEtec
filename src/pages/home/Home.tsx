31ebea4fa0efdbe698175a16938dea1457e28b51
ferreiraalan13 <ferreiraalan13@gmail.com>
2024-04-30 22:32:13 -0300

    remocao de barra de rolagem

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,35 +1,52 @@
 import Post from "../../components/Post";
+
 import { Box } from "@chakra-ui/react";
+
 import MenuFinal from "../../components/MenuFinal";
 import Eventos from "../../components/Evento";
 
 export default function App() {
   return (
     <div className="flex justify-center bg-gray-300">
-      <Box
-        height={"100vh"}
-        overflowY={"scroll"} // Mantém a funcionalidade de rolagem
-        display={"flex"}
-        // Utilize a propriedade CSS `::-webkit-scrollbar` para ocultar a barra de rolagem
-        css={{
-          "::-webkit-scrollbar": {
-            display: "none"
-          }
-        }}
-      >
-        <MenuFinal />
+    <Box
+      height={"100vh"}
+      overflow={"hidden"}
+      gap={0}
+      className="flex bg-gray-300"
+      display={'flex'}
+    >
+      <MenuFinal />
+      
 
-        <Box padding={"20px"} overflowX={"hidden"} width={"100%"} >
-          <Box display={"flex"} justifyContent={""} gap={3}>
-            <Box display={"flex"} flexDirection={"column"} gap={3}>
-              <Post />
-            </Box>
-            <Box>
-              <Eventos />
-            </Box>
+      <Box
+        padding={"20px"}
+        css={{
+          "&::-webkit-scrollbar": {
+            width: "0",
+            height: "0",
+            borderRadius: "20px",
+            backgroundColor: "darkgray",
+            marginRight: "4px",
+            background: "transparent"
+          },
+          "&::-webkit-scrollbar-thumb": {
+            background: "grey",
+            borderRadius: "20px",
+          },
+        }}
+        overflowX={"hidden"}
+        width={"100%"}
+      >
+        <Box display={"flex"} justifyContent={""} gap={3}>
+          <Box display={"flex"} flexDirection={"column"} gap={3}>
+            <Post />
+          </Box>
+          <Box>
+            <Eventos />
           </Box>
         </Box>
       </Box>
+    </Box>
     </div>
   );
 }
